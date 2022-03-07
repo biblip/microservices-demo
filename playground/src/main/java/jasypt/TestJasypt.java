d2c5c0779c9e61e7c50f22714b4d048c82be5f00
Arturo <arturo@cuplease.com>
2022-03-07 17:58:43 -0500

    Add Secrets to all passwords and required fields on application.yml, and add environment variables to containers

--- playground/src/main/java/jasypt/TestJasypt.java
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -6,10 +6,10 @@ import org.jasypt.iv.RandomIvGenerator;
 public class TestJasypt {
     public static void main(String[] args) {
         StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
-        standardPBEStringEncryptor.setPassword("Demo_Pwd!2022");
+        standardPBEStringEncryptor.setPassword(System.getenv("JASYPT_ENCRYPTOR_PASSWORD"));
         standardPBEStringEncryptor.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
-        String result = standardPBEStringEncryptor.encrypt("springCloud_Pwd!");
+        String result = standardPBEStringEncryptor.encrypt("sometext");
 
         System.out.println("Encrypted Result : " + result);
         System.out.println("Decrypted Result : " + standardPBEStringEncryptor.decrypt(result));
